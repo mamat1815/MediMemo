cfd5cdff8c93c0dec0b2b0022cdb9af0ef0642d6
mamat1815 <mbahgendengngegame@gmail.com>
2023-11-27 14:28:14 +0700

    Add Google Sign In Options

--- app/src/main/java/com/medimemo/ui/signin/SignInActivity.kt
+++ app/src/main/java/com/medimemo/ui/signin/SignInActivity.kt
@@ -5,7 +5,15 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Patterns
 import android.widget.Toast
+import com.google.android.gms.auth.api.Auth
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
+import com.google.android.gms.tasks.Task
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.GoogleAuthProvider
 import com.medimemo.MainActivity
 import com.medimemo.databinding.ActivitySignInBinding
 import com.medimemo.ui.signup.SignUpActivity
@@ -14,12 +22,17 @@ class SignInActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivitySignInBinding
     private lateinit var auth : FirebaseAuth
+    //Login Google
+    private val googleSignInClient by lazy { configureGoogleSignIn() }
+    private val RC_SIGN_IN = 9001
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivitySignInBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         auth = FirebaseAuth.getInstance()
+        
+
 
         binding.apply {
 
@@ -60,6 +73,11 @@ class SignInActivity : AppCompatActivity() {
                 startActivity(intent)
             }
 
+            btnLgGso.setOnClickListener {
+                val signInIntent = googleSignInClient.signInIntent
+                startActivityForResult(signInIntent, RC_SIGN_IN)
+            }
+
         }
     }
 
@@ -75,4 +93,50 @@ class SignInActivity : AppCompatActivity() {
                 }
             }
     }
+
+    private fun configureGoogleSignIn(): GoogleSignInClient {
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken("648278628664-9ssgfav84gimrunihbv8h2465q79q98d.apps.googleusercontent.com")
+            .requestEmail()
+            .build()
+        return GoogleSignIn.getClient(this, gso)
+    }
+
+    @Deprecated("Deprecated in Java")
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == RC_SIGN_IN) {
+            val result = Auth.GoogleSignInApi.getSignInResultFromIntent(data)
+            if (result.isSuccess) {
+                // Sign-in berhasil, authentikasi dengan Firebase
+                val account = result.signInAccount
+                firebaseAuthWithGoogle(account!!)
+            } else {
+                // Sign-in gagal, mungkin pengguna membatalkan atau terjadi kesalahan
+                // Penanganan kesalahan dapat dilakukan di sini
+                Toast.makeText(this, "Sign In Failed", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+    }
+
+    private fun firebaseAuthWithGoogle(account: GoogleSignInAccount?) {
+        val credential = GoogleAuthProvider.getCredential(account?.idToken, null)
+        auth.signInWithCredential(credential)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    Toast.makeText(this, "Selamat Datang ${auth.currentUser?.displayName}", Toast.LENGTH_SHORT).show()
+                    val intent = Intent(this@SignInActivity, MainActivity::class.java)
+                    startActivity(intent)
+                } else {
+                    Toast.makeText(this, "${task.exception?.message}", Toast.LENGTH_SHORT).show()
+                }
+
+            }
+    }
+
+
+
+
 }
